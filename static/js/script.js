7871938b4dabe8f6f88bad97e7e35f4797c0c51c
Okan Bakırcı <132247985+bkrcokan13@users.noreply.github.com>
2025-05-13 20:15:09 +0300

    Refactor extensions list rendering and improve filter button functionality

--- static/js/script.js
+++ static/js/script.js
@@ -9,8 +9,8 @@ let extensionsAll = [];
 
 // Theme Button Icons
 const themeBtnIcons = {
-    'light' : '/assets/images/icon-moon.svg',
-    'dark' : '/assets/images/icon-sun.svg'
+    'light': '/assets/images/icon-moon.svg',
+    'dark': '/assets/images/icon-sun.svg'
 };
 document.addEventListener('DOMContentLoaded', () => {
     // Initialize Data Fetch
@@ -18,30 +18,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Initialize Theme Icon
     themeBtnIcon.src = document.body.classList.contains('dark') ? themeBtnIcons.dark : themeBtnIcons.light;
-    themeBtn.addEventListener('click' , lightDarkTheme);
+    themeBtn.addEventListener('click', lightDarkTheme);
 
     // Initialize Filter Button
-    filterButtons.forEach((filterButton,idx) => {
-
+    filterButtons.forEach((filterButton, idx) => {
         // Filter Button Click Event
         filterButton.addEventListener('click', () => {
             let filteredData = extensionsAll;
-
-
             // Check Active Status
-            if(filterButton.classList.contains("active")) {
+            if (filterButton.classList.contains("active")) {
                 return;
             }
             filterButtons.forEach(btn => btn.classList.remove("active"));
             filterButton.classList.add("active");
-
             // Clear Extensions Body
             switch (idx) {
                 case 0:
                     extensionBody.innerHTML = "";
-
                     filteredData.forEach((d) => {
-                        extensionBody.innerHTML += "<div class=\"extension\"></div>\n"
+                        extensionBody.innerHTML += `
+                                <div class="extension">
+                                    <div class="extension-box-container">
+                                          <div class="extensions-info">
+                                            <div class="extension-icon">
+                                              <img src="${d.logo}" alt="">
+                                            </div>
+                                            <div class="extensions-details">
+                                              <h3>${d.name}</h3>
+                                              <p>${d.description}</p>
+                                            </div>
+                                          </div>
+                                          <div class="extensions-buttons">
+                                            <a href="javascript:void(0)" class="remove-btn">Remove</a>
+                                            <label class="active-btn">
+                                              <input type="checkbox" ${d.isActive ? "checked" : ""}>
+                                              <span class="slider"></span>
+                                            </label>
+                                          </div>
+                                    </div>                                       
+                              </div> `;
+
+                        console.log(filteredData.name);
                     });
                     break;
                 case 1:
@@ -50,8 +67,29 @@ document.addEventListener('DOMContentLoaded', () => {
                     // Only Active
                     filteredData = extensionsAll.filter(val => val.isActive);
                     filteredData.forEach(d => {
-                        extensionBody.innerHTML += "<div class=\"extension\"></div>\n"
-                    })
+                        extensionBody.innerHTML += `
+                             <div class="extension">
+                                    <div class="extension-box-container">
+                                          <div class="extensions-info">
+                                            <div class="extension-icon">
+                                              <img src="${d.logo}" alt="">
+                                            </div>
+                                            <div class="extensions-details">
+                                              <h3>${d.name}</h3>
+                                              <p>${d.description}</p>
+                                            </div>
+                                          </div>
+                                          <div class="extensions-buttons">
+                                            <a href="javascript:void(0)" class="remove-btn">Remove</a>
+                                            <label class="active-btn">
+                                              <input type="checkbox" ${d.isActive ? "checked" : ""}>
+                                              <span class="slider"></span>
+                                            </label>
+                                          </div>
+                                    </div>                                       
+                              </div>
+                        `;
+                    });
                     break;
                 case 2:
                     extensionBody.innerHTML = "";
@@ -59,7 +97,28 @@ document.addEventListener('DOMContentLoaded', () => {
                     // Only InActive
                     filteredData = extensionsAll.filter(val => !val.isActive);
                     filteredData.forEach(d => {
-                        extensionBody.innerHTML += "<div class=\"extension\"></div>\n"
+                        extensionBody.innerHTML += `
+                             <div class="extension">
+                                    <div class="extension-box-container">
+                                          <div class="extensions-info">
+                                            <div class="extension-icon">
+                                              <img src="${d.logo}" alt="">
+                                            </div>
+                                            <div class="extensions-details">
+                                              <h3>${d.name}</h3>
+                                              <p>${d.description}</p>
+                                            </div>
+                                          </div>
+                                          <div class="extensions-buttons">
+                                            <a href="javascript:void(0)" class="remove-btn">Remove</a>
+                                            <label class="active-btn">
+                                              <input type="checkbox" ${d.isActive ? "": ""}>
+                                              <span class="slider"></span>
+                                            </label>
+                                          </div>
+                                    </div>                                       
+                              </div>
+                        `;
                     });
                     break;
             }
@@ -74,17 +133,17 @@ async function initExtensions() {
     await setActiveFilterButton(0);
 }
 // Fetch Extensions Data
- async function getExtensionsData() {
+async function getExtensionsData() {
     try {
         const res = await fetch("data.json");
 
-        if(!res.ok) {
+        if (!res.ok) {
             console.error(`Error, extensions is not fetched ! \n\r Status : ${res.status}`);
         }
 
         return await res.json();
     }
-    catch(error) {
+    catch (error) {
         console.error(error);
         return null;
     }
@@ -95,13 +154,33 @@ async function initExtensions() {
 function setActiveFilterButton(btnIndex) {
     let allData = extensionsAll;
     try {
-        filterButtons.forEach((val,idx) => {
+        filterButtons.forEach((val, idx) => {
             if (btnIndex === idx) {
                 val.classList.toggle("active", true);
                 // Clear Body
                 extensionBody.innerHTML = "";
                 allData.forEach(data => {
-                        extensionBody.innerHTML += "<div class=\"extension\"></div>\n"
+                    extensionBody.innerHTML += `
+                                <div class="extension">
+                                    <div class="extension-box-container">
+                                          <div class="extensions-info">
+                                            <div class="extension-icon">
+                                              <img src="${data.logo}" alt="">
+                                            </div>
+                                            <div class="extensions-details">
+                                              <h3>${data.name}</h3>
+                                              <p>${data.description}</p>
+                                            </div>
+                                          </div>
+                                          <div class="extensions-buttons">
+                                            <a href="javascript:void(0)" class="remove-btn">Remove</a>
+                                            <label class="active-btn">
+                                              <input type="checkbox" ${data.isActive ? "checked" : ""}>
+                                              <span class="slider"></span>
+                                            </label>
+                                          </div>
+                                    </div>                                       
+                              </div> `;
                 });
             }
         });
